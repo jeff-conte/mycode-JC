5f83d249f34bf5760bc0f0ffa78484e447c2f540
jeff.conte@rotary.org <jeff.conte@rotary.org>
2024-07-11 16:21:01 +0000

    terraform and Azure virtual networks

---
+++ azure-tf-tour/variables.tf
@@ -0,0 +1,16 @@
+# this is now an "input" value 
+variable "build_location" {
+  type = string
+  default = "westus2"
+}
+
+# this is now an "input" value 
+variable "rg_name" {
+  type = string
+  default = "myAzureRG"
+}
+# this is now an "input" value
+variable "vm_net_name" {
+  type = string
+  default = "myAzureVNET"
+}
---
+++ azure-vnet/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+    type        = string
+    default     = "testy"
+    description = "Name for this infrastructure"
+}
+
+variable "location" {
+    type         = string
+    description  = "Name of the region for this infrastructure"
+    default      = "westus2"
+}
+
+variable "address_space" {
+    type        = string
+    description = "Cidr range for the Virtual Network"
+}
